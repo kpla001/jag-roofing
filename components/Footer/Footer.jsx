8b76f23fd0d241c7aecadd556aaad7b1d7e3a5ff
kpla001 <kpla001@fiu.edu>
2022-01-07 11:39:49 -0500

    (styles): change footer section to row for larger screens; add left&right padding for navbar/footer; change header font to helvetica neue

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,27 +3,29 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.contact}>
-        <h1>Contact</h1>
-        <i className="fas fa-phone "></i>
-        &nbsp;
-        <p>(305)484-1080</p>
-        <br />
-        <i className="fas fa-envelope"></i>
-        &nbsp;
-        <p>garciajoe@jagroofing.com</p>
-      </div>
-      <div className={styles.hours}>
-        <h1>Business Hours</h1>
-        <div className={styles.schedule}>
-          <p>
-            <span className={styles.column1}>Monday - Friday:</span>
-            <span> 8:00am - 5:30pm </span>
-          </p>
-          <p>
-            <span className={styles.column1}>Saturday & Sunday:</span>
-            <span>Closed</span>
-          </p>
+      <div className={styles.footerSplit}>
+        <div className={styles.contact}>
+          <h1>Contact</h1>
+          <i className="fas fa-phone "></i>
+          &nbsp;
+          <p>(305)484-1080</p>
+          <br />
+          <i className="fas fa-envelope"></i>
+          &nbsp;
+          <p>garciajoe@jagroofing.com</p>
+        </div>
+        <div className={styles.hours}>
+          <h1>Business Hours</h1>
+          <div className={styles.schedule}>
+            <p>
+              <span className={styles.column1}>Monday - Friday:</span>
+              <span> 8:00am - 5:30pm </span>
+            </p>
+            <p>
+              <span className={styles.column1}>Saturday & Sunday:</span>
+              <span>Closed</span>
+            </p>
+          </div>
         </div>
         <div className={styles.license}>
           <h1>License #</h1>
