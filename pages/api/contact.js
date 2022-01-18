549e4bd5779864239f34759fe193b1175db51317
kpla001 <kpla001@fiu.edu>
2022-01-18 12:47:34 -0500

    (prod): complete mailer bot functionality for contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,27 @@
 export default function (req, res) {
-  console.log(req.body)
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+      user: 'bot.jagroofing@gmail.com',
+      pass: `${process.env.PASSWORD}`,
+    },
+    secure: true,
+  })
+
+  const mailData = {
+    from: 'bot.jagroofing@gmail.com',
+    to: 'bot.jagroofing@gmail.com',
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + ' | Sent from: ' + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`,
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err)
+    else console.log(info)
+  })
+  res.status(200)
 }
